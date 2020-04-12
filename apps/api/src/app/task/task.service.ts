588aeb32f84e786e6b9a7554b125f676363a77b3
Boris <boris@192.168.1.5>
2020-04-12 18:29:32 +0200

    Convert to NX

---
+++ apps/api/src/app/task/task.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import * as uuid from 'uuid/v1';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TaskService {
+  private tasks: Task[] = [];
+
+  findAll(): Task[] {
+    return this.tasks;
+  }
+
+  create(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks = [...this.tasks, task];
+    return task;
+  }
+
+  findById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+}
